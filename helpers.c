41ccc49fe70af0a51a79eb1b5d0aceac0f310569
Kiana Khalili <96719533+Kiana02@users.noreply.github.com>
2023-10-02 00:09:08 +0330

    filter.c
    Implement the functions in helpers.c such that a user can apply grayscale, sepia, reflection, or blur filters to their images.
    
    The function grayscale should take an image and turn it into a black-and-white version of the same image.
    The function sepia should take an image and turn it into a sepia version of the same image.
    The reflect function should take an image and reflect it horizontally.
    Finally, the blur function should take an image and turn it into a box-blurred version of the same image.
    You should not modify any of the function signatures, nor should you modify any other files other than helpers.c. 
    P.S This is an assignment I did from the CS50 course I took.

---
+++ helpers.c
@@ -0,0 +1,121 @@
+#include "helpers.h"
+#include <math.h>
+#include <stdio.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    int sr, sg, sb;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sr = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            if (sr > 255)
+            {
+                sr = 255;
+            }
+            sg = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            if (sg > 255)
+            {
+                sg = 255;
+            }
+            sb = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+            if (sb > 255)
+            {
+                sb = 255;
+            }
+
+            image[i][j].rgbtRed = sr;
+            image[i][j].rgbtGreen = sg;
+            image[i][j].rgbtBlue = sb;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE tmp;
+    for (int j = 0; j < height; j++)
+    {
+            for (int i = 0; i <= ((width - 1) / 2); i++)
+            {
+                tmp = image[j][i];
+                image[j][i] = image[j][width - 1 - i];
+                image[j][width - 1 - i] = tmp;
+            }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sumBlue = 0;
+            float sumGreen = 0;
+            float sumRed = 0;
+            float counter = 0;
+
+            for (int r = -1; r < 2; r++)
+            {
+                for (int c = -1; c < 2; c++)
+                {
+                    if (i + r < 0 || i + r > height - 1)
+                    {
+                        continue;
+                    }
+
+                    if (j + c < 0 || j + c > width - 1)
+                    {
+                        continue;
+                    }
+
+                    sumBlue += image[i + r][j + c].rgbtBlue;
+                    sumGreen += image[i + r][j + c].rgbtGreen;
+                    sumRed += image[i + r][j + c].rgbtRed;
+                    counter++;
+                }
+            }
+
+            temp[i][j].rgbtBlue = round(sumBlue / counter);
+            temp[i][j].rgbtGreen = round(sumGreen / counter);
+            temp[i][j].rgbtRed = round(sumRed / counter);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+        }
+    }
+
+    return;
+}
